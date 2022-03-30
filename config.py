54eece1ac02d7e4becb0b2f25b4f790886f0fe6b
htdmirrorleechonheroku <101859676+htdmirrorleechonheroku@users.noreply.github.com>
2022-03-30 15:20:30 +0630

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("5213193325:AAEEuiqQO1hrzWaOpv542biBTXApClyt8YI","")
-    HEROKU_API_KEY = os.environ.get("dd26273b-f962-4eef-8c23-e3e429af703d","")
-    AUTHORIZED_USERS = [int(user) for user in os.environ.get("5108941922").split(" ")]
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","")
+    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY","")
+    AUTHORIZED_USERS = [int(user) for user in os.environ.get("AUTHORIZED_USERS").split(" ")]
     TG_CHARACTER_LIMIT = 4000 
-    HEROKU_APP_NAME = os.environ.get("htdherokumanager","")
+    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME","")
